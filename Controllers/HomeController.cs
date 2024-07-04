bb5dea5594a91c09098e26dd4a5ba42d433b544a
nurlanaismayilova <150562064+nurlanaismayilova@users.noreply.github.com>
2024-07-04 17:29:49 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sitee.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
